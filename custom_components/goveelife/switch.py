a48727ec9028ad6115e79236ee456d1ffa94b22e
disforw <github@benzona.net>
2024-04-30 13:36:36 -0400

    Update switch.py

--- custom_components/goveelife/switch.py
+++ custom_components/goveelife/switch.py
@@ -36,12 +36,12 @@ from .utils import (
 _LOGGER: Final = logging.getLogger(__name__)
 platform='switch'
 platform_device_types = [ 
-    'devices.types.heater:.*on_off:.*',
-    'devices.types.heater:.*toggle:oscillationToggle',
+    #'devices.types.heater:.*on_off:.*',
+    'devices.types.heater:.*toggle:.*',
     'devices.types.socket:.*on_off:.*', 
     'devices.types.socket:.*toggle:.*', 
     'devices.types.light:.*toggle:gradientToggle'
-    #'devices.types.air_purifier:.*on_off:.*'
+    'devices.types.air_purifier:.*on_off:.*'
 ]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
