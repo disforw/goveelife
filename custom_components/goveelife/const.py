c2a7104867386bbf533be13ddce84d7636aa8811
B A <github@benzona.net>
2024-05-11 21:10:25 -0400

    Add unit switch to flow

--- custom_components/goveelife/const.py
+++ custom_components/goveelife/const.py
@@ -17,6 +17,7 @@ EVENT_PROPS_ID: Final = DOMAIN + '_property_message'
 CONF_COORDINATORS: Final = 'coordinators'
 CONF_API_COUNT: Final = 'api_count'
 CONF_ENTRY_ID: Final = 'entry_id'
+CONF_CLIMATE_UNIT: Final = 'unit'
 
 CLOUD_API_URL_DEVELOPER: Final = 'https://developer-api.govee.com/v1/appliance/devices/'
 CLOUD_API_URL_OPENAPI: Final = 'https://openapi.api.govee.com/router/api/v1'
