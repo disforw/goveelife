610df90fe377dd907dcfb38043a00d7869a693e3
disforw <github@benzona.net>
2024-07-11 21:40:20 -0400

    Update diagnostics.py

--- custom_components/goveelife/diagnostics.py
+++ custom_components/goveelife/diagnostics.py
@@ -1,65 +1,56 @@
 """Diagnostics support for the Govee Life integration."""
 
-from __future__ import annotations
-from importlib_metadata import version
-from typing import (
-    Final,
-    Any,
-)
+from typing import Final, Any
 import logging
-import asyncio
+from importlib_metadata import version
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.components.diagnostics import async_redact_data
-from homeassistant.const import (
-    CONF_API_KEY,
-    CONF_DEVICES,
-    CONF_RESOURCE,
-    CONF_STATE,
-)
+from homeassistant.const import CONF_API_KEY, CONF_DEVICES, CONF_STATE
 
-from .const import (
-    DOMAIN,
-)
+from .const import DOMAIN
 
 REDACT_CONFIG = {CONF_API_KEY}
-REDACT_CLOUD_DEVICES = {'dummy1', 'dummy2' }
-REDACT_CLOUD_STATES = {'dummy1', 'dummy2' }
+REDACT_CLOUD_DEVICES = {'dummy1', 'dummy2'}
+REDACT_CLOUD_STATES = {'dummy1', 'dummy2'}
 
 _LOGGER: Final = logging.getLogger(__name__)
-platform='diagnostics'
+platform = 'diagnostics'
 
-async def async_get_config_entry_diagnostics( hass: HomeAssistant, entry: ConfigEntry ) -> dict[str, Any]:
+async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry) -> dict:
     """Return diagnostics for a config entry."""
-    _LOGGER.debug("Returning %s platform entry: %s", platform, entry.entry_id) 
+    _LOGGER.debug("Returning %s platform entry: %s", platform, entry.entry_id)
+    diag = {}
+    
     try:
-        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Add config entry configuration to output", entry.entry_id, platform)
-        diag: dict[str, Any] = { "config": async_redact_data(entry.as_dict(), REDACT_CONFIG) }
+        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Adding config entry configuration to output", entry.entry_id, platform)
+        diag["config"] = async_redact_data(entry.as_dict(), REDACT_CONFIG)
     except Exception as e:
         _LOGGER.error("%s - async_get_config_entry_diagnostics %s: Adding config entry configuration to output failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
-        #return False
+        # return False
 
     entry_data = hass.data[DOMAIN][entry.entry_id]
-    try:
-        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Add cloud received device list", entry.entry_id, platform)
-        diag["cloud_devices"] = async_redact_data(entry_data[CONF_DEVICES], REDACT_CLOUD_DEVICES)
-    except Exception as e:
-        _LOGGER.error("%s - async_get_config_entry_diagnostics %s: Add cloud received device list failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
-        #return False
-        
-    try:
-        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Add cloud received device states", entry.entry_id, platform)
-        diag["cloud_states"] = async_redact_data(entry_data[CONF_STATE], REDACT_CLOUD_STATES)
-    except Exception as e:
-        _LOGGER.error("%s - async_get_config_entry_diagnostics %s: Add cloud received device states: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
-        #return False
 
     try:
-        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Add python module [goveelife] version", entry.entry_id, platform)
+        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Adding cloud received device list", entry.entry_id, platform)
+        diag["cloud_devices"] = async_redact_data(entry_data.get(CONF_DEVICES, {}), REDACT_CLOUD_DEVICES)
+    except Exception as e:
+        _LOGGER.error("%s - async_get_config_entry_diagnostics %s: Adding cloud received device list failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
+        # return False
+
+    try:
+        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Adding cloud received device states", entry.entry_id, platform)
+        diag["cloud_states"] = async_redact_data(entry_data.get(CONF_STATE, {}), REDACT_CLOUD_STATES)
+    except Exception as e:
+        _LOGGER.error("%s - async_get_config_entry_diagnostics %s: Adding cloud received device states failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
+        # return False
+
+    try:
+        _LOGGER.debug("%s - async_get_config_entry_diagnostics %s: Adding Python module [goveelife] version", entry.entry_id, platform)
         diag["py_module_requests"] = version('requests')
     except Exception as e:
-        _LOGGER.error("%s - async_get_config_entry_diagnostics %s: Add python module [goveelife] version failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
-        #return False
+        _LOGGER.error("%s - async_get_config_entry_diagnostics %s: Adding Python module [goveelife] version failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
+        # return False
 
     return diag
