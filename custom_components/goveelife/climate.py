49246adfc533a8a2be7d1631fc59fee5111b7feb
disforw <github@benzona.net>
2024-07-12 18:21:16 -0400

    Update climate.py

--- custom_components/goveelife/climate.py
+++ custom_components/goveelife/climate.py
@@ -5,9 +5,7 @@ from typing import Final
 import logging
 import asyncio
 
-from homeassistant.core import (
-    HomeAssistant,
-)
+from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.components.climate import (
     ClimateEntity,
@@ -15,67 +13,52 @@ from homeassistant.components.climate import (
     HVACMode,
 )
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.const import (
     CONF_DEVICES,
     STATE_UNKNOWN,
     UnitOfTemperature,
 )
 
-from .entities import (
-    GoveeLifePlatformEntity,
-)
-
-from .const import (
-    DOMAIN,
-    CONF_COORDINATORS,
-)
-
-from .utils import (
-    GoveeAPI_GetCachedStateValue,
-    async_GoveeAPI_ControlDevice,
-)
+from .entities import GoveeLifePlatformEntity
+from .const import DOMAIN, CONF_COORDINATORS
+from .utils import GoveeAPI_GetCachedStateValue, async_GoveeAPI_ControlDevice
 
 _LOGGER: Final = logging.getLogger(__name__)
-platform='climate'
-platform_device_types = [ 'devices.types.heater' ]
+PLATFORM = 'climate'
+PLATFORM_DEVICE_TYPES = ['devices.types.heater']
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
     """Set up the climate platform."""
-    _LOGGER.debug("Setting up %s platform entry: %s | %s", platform, DOMAIN, entry.entry_id)
-    entites=[]    
-    
+    _LOGGER.debug("Setting up %s platform entry: %s | %s", PLATFORM, DOMAIN, entry.entry_id)
+    entities = []
+
     try:
-        _LOGGER.debug("%s - async_setup_entry %s: Getting cloud devices from data store", entry.entry_id, platform)
-        entry_data=hass.data[DOMAIN][entry.entry_id]
-        api_devices=entry_data[CONF_DEVICES]
+        entry_data = hass.data[DOMAIN][entry.entry_id]
+        api_devices = entry_data[CONF_DEVICES]
     except Exception as e:
-        _LOGGER.error("%s - async_setup_entry %s: Getting cloud devices from data store failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
-        return False
+        _LOGGER.error("%s - async_setup_entry %s: Failed to get cloud devices from data store: %s (%s.%s)", entry.entry_id, PLATFORM, str(e), e.__class__.__module__, type(e).__name__)
+        return
 
     for device_cfg in api_devices:
         try:
-            if not device_cfg.get('type',STATE_UNKNOWN) in platform_device_types:
-                continue      
-            d=device_cfg.get('device')
-            _LOGGER.debug("%s - async_setup_entry %s: Setup device: %s", entry.entry_id, platform, d) 
-            coordinator = entry_data[CONF_COORDINATORS][d]
-            entity=GoveeLifeClimate(hass, entry, coordinator, device_cfg, platform=platform)
-            entites.append(entity)
+            if device_cfg.get('type') not in PLATFORM_DEVICE_TYPES:
+                continue
+            device = device_cfg.get('device')
+            coordinator = entry_data[CONF_COORDINATORS][device]
+            entity = GoveeLifeClimate(hass, entry, coordinator, device_cfg, platform=PLATFORM)
+            entities.append(entity)
             await asyncio.sleep(0)
         except Exception as e:
-            _LOGGER.error("%s - async_setup_entry %s: Setup device failed: %s (%s.%s)", entry.entry_id, platform, str(e), e.__class__.__module__, type(e).__name__)
-            return False
+            _LOGGER.error("%s - async_setup_entry %s: Failed to setup device: %s (%s.%s)", entry.entry_id, PLATFORM, str(e), e.__class__.__module__, type(e).__name__)
+            return
 
-    _LOGGER.info("%s - async_setup_entry: setup %s %s entities", entry.entry_id, len(entites), platform)
-    if not entites:
-        return None
-    async_add_entities(entites)
+    if entities:
+        async_add_entities(entities)
 
 
 class GoveeLifeClimate(ClimateEntity, GoveeLifePlatformEntity):
     """Climate class for Govee Life integration."""
-    
+
     _attr_hvac_modes = []
     _attr_hvac_modes_mapping = {}
     _attr_hvac_modes_mapping_set = {}
@@ -85,9 +68,9 @@ class GoveeLifeClimate(ClimateEntity, GoveeLifePlatformEntity):
     _enable_turn_on_off_backwards_compatibility = False
 
     def _init_platform_specific(self, **kwargs):
-        """Platform specific init actions"""
+        """Platform specific init actions."""
         _LOGGER.debug("%s - %s: _init_platform_specific", self._api_id, self._identifier)
-        capabilities = self._device_cfg.get('capabilities',[])
+        capabilities = self._device_cfg.get('capabilities', [])
 
         _LOGGER.debug("%s - %s: _init_platform_specific: processing devices request capabilities", self._api_id, self._identifier)
         for cap in capabilities:
@@ -96,19 +79,19 @@ class GoveeLifeClimate(ClimateEntity, GoveeLifePlatformEntity):
                 for option in cap['parameters']['options']:
                     if option['name'] == 'on':
                         self._attr_supported_features |= ClimateEntityFeature.TURN_ON
-                        self._attr_hvac_modes += [ HVACMode.HEAT_COOL ]
+                        self._attr_hvac_modes.append(HVACMode.HEAT_COOL)
                         self._attr_hvac_modes_mapping[option['value']] = HVACMode.HEAT_COOL
                         self._attr_hvac_modes_mapping_set[HVACMode.HEAT_COOL] = option['value']
                     elif option['name'] == 'off':
                         self._attr_supported_features |= ClimateEntityFeature.TURN_OFF
-                        self._attr_hvac_modes += [ HVACMode.OFF ]
+                        self._attr_hvac_modes.append(HVACMode.OFF)
                         self._attr_hvac_modes_mapping[option['value']] = HVACMode.OFF
                         self._attr_hvac_modes_mapping_set[HVACMode.OFF] = option['value']
                     else:
                         _LOGGER.warning("%s - %s: _init_platform_specific: unknown on_off option: %s", self._api_id, self._identifier, option)
             elif cap['type'] == 'devices.capabilities.temperature_setting' and cap['instance'] == 'targetTemperature':
                 self._attr_supported_features |= ClimateEntityFeature.TARGET_TEMPERATURE
-                for field in cap['parameters']['fields']:                  
+                for field in cap['parameters']['fields']:
                     if field['fieldName'] == 'temperature':
                         self._attr_max_temp = field['range']['max']
                         self._attr_min_temp = field['range']['min']
@@ -170,9 +153,11 @@ class GoveeLifeClimate(ClimateEntity, GoveeLifePlatformEntity):
         return None
 
     async def async_turn_off(self) -> None:
+        """Turn the entity off."""
         await self.async_set_hvac_mode(HVACMode.OFF)
-    
+
     async def async_turn_on(self) -> None:
+        """Turn the entity on."""
         await self.async_set_hvac_mode(HVACMode.HEATING)
 
     @property
@@ -204,28 +189,26 @@ class GoveeLifeClimate(ClimateEntity, GoveeLifePlatformEntity):
     def temperature_unit(self) -> str:
         """Return the temperature unit of the entity."""
         value = GoveeAPI_GetCachedStateValue(self.hass, self._entry_id, self._device_cfg.get('device'), 'devices.capabilities.temperature_setting', 'targetTemperature')
-        u = value.get('unit','CELSIUS').upper()
-        return UnitOfTemperature[u]
+        return UnitOfTemperature[value.get('unit', 'CELSIUS').upper()]
 
     @property
     def target_temperature(self) -> float | None:
         """Return the target temperature of the entity."""
         #_LOGGER.debug("%s - %s: target_temperature", self._api_id, self._identifier)
         value = GoveeAPI_GetCachedStateValue(self.hass, self._entry_id, self._device_cfg.get('device'), 'devices.capabilities.temperature_setting', 'targetTemperature')
-        t = value.get('targetTemperature',0)
-        return t
+        return value.get('targetTemperature', 0)
 
     async def async_set_temperature(self, **kwargs) -> None:
         """Set new target temperature."""        
         #_LOGGER.debug("%s - %s: async_set_temperature", self._api_id, self._identifier)
         value = GoveeAPI_GetCachedStateValue(self.hass, self._entry_id, self._device_cfg.get('device'), 'devices.capabilities.temperature_setting', 'targetTemperature')
-        u = value.get('unit','Celsius')        
+        unit = value.get('unit', 'Celsius')
         state_capability = {
             "type": "devices.capabilities.temperature_setting",
             "instance": "targetTemperature",
             "value": {
                 "temperature": kwargs['temperature'],
-                "unit": u,
+                "unit": unit,
                 }
             }
         if await async_GoveeAPI_ControlDevice(self.hass, self._entry_id, self._device_cfg, state_capability):
@@ -239,7 +222,8 @@ class GoveeLifeClimate(ClimateEntity, GoveeLifePlatformEntity):
         #_LOGGER.debug("%s - %s: current_temperature", self._api_id, self._identifier)  
         value = GoveeAPI_GetCachedStateValue(self.hass, self._entry_id, self._device_cfg.get('device'), 'devices.capabilities.property', 'sensorTemperature')
         if self.temperature_unit == UnitOfTemperature.CELSIUS:
-            value = (value-32)*5/9 #value seems to be always Fahrenheit - calculate to °C if necessary
+            #value seems to be always Fahrenheit - calculate to °C if necessary
+            value = (value - 32) * 5 / 9
         return value
 
 
